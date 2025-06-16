07b23fac16855da3158d073c86c57256b42c4027
RowanLivingstone <1livingstrow@hdsb.ca>
2025-06-16 13:06:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 /**** SETUP CODE ****/
+const objs = [];
+let currentCircle = 0;
 
 // Call the init function when the HTML window loads
 window.onload = init;
@@ -16,22 +18,36 @@ function init() {
 
 // Write the circle object function here
 
-
+function Circle(x, y, color) {
+  this.x = x;
+  this.y = y;
+  this.color = color
+}
 
 // Write the createCircle function here. 
 
+function createCircle() {
 
+  objs[currentCircle] = new Circle(randomInteger(0,480), randomInteger(480), 'red');
+
+  ctx.beginPath();
+  ctx.arc(objs[currentCircle].x, objs[currentCircle].y, 10, 0, Math.PI * 2);
+  ctx.closePath();
+  ctx.fillStyle = objs[currentCircle].color;
+  ctx.fill();
+}
 
 // Write the randomInteger function here. 
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 
 
 /**** GAMELOOP ****/
-
 function gameLoop(timestamp) {
-
   // Call the createCircle function here.
-
+  createCircle();
 
   // This causes the game to loop every frame (Do not change this)
   window.requestAnimationFrame(gameLoop);
